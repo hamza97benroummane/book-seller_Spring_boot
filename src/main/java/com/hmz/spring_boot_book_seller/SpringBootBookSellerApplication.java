30f8221c01fbd0edbc79f5b7e05068cbf76f86cc
hamza97benroummane <hamza.benroummane@gmail.com>
2024-11-12 09:55:14 +0100

    upload project

---
+++ src/main/java/com/hmz/spring_boot_book_seller/SpringBootBookSellerApplication.java
@@ -0,0 +1,13 @@
+package com.hmz.spring_boot_book_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmz/spring_boot_book_seller/SpringBootBookSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmz.spring_boot_book_seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
